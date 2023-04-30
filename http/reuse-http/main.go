a8ec375af99dafdc73e0d5808870a17dd8b547ce
phucduc <phucducktpm@gmail.com>
2023-04-30 19:37:16 +0700

    feat: reuse http client

---
+++ http/reuse-http/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptrace"
+)
+
+func main() {
+	Reuse()
+	//
+	NonReuse()
+}
+
+// NonReuse, not reuse http
+func NonReuse() {
+	// client trace to log whether the request's underlying tcp connection was re-used
+	clientTrace := &httptrace.ClientTrace{
+		GotConn: func(info httptrace.GotConnInfo) {
+			log.Printf("conn was reused: %t", info.Reused)
+		},
+	}
+	traceCtx := httptrace.WithClientTrace(context.Background(), clientTrace)
+
+	// 1st request
+	req, err := http.NewRequestWithContext(traceCtx, http.MethodGet, "http://example.com", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// 2nd request
+	req, err = http.NewRequestWithContext(traceCtx, http.MethodGet, "http://example.com", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Reuse, reuse http client
+func Reuse() {
+	var (
+		err error
+	)
+	// client trace to log whether the request's underlying tcp connection was re-used
+	clientTrace := &httptrace.ClientTrace{
+		GotConn: func(info httptrace.GotConnInfo) {
+			log.Printf("conn was reused: %t", info.Reused)
+		},
+	}
+	traceCtx := httptrace.WithClientTrace(context.Background(), clientTrace)
+
+	// 1st request
+	req, err := http.NewRequestWithContext(traceCtx, http.MethodGet, "http://example.com", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := io.Copy(ioutil.Discard, res.Body); err != nil {
+		log.Fatal(err)
+	}
+	res.Body.Close()
+	// 2nd request
+	req, err = http.NewRequestWithContext(traceCtx, http.MethodGet, "http://example.com", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
